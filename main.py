0e3cca8ff8dee2ba732c59ef8c0efdcb1b16e316
D1ther <maxnozhkin2006@gmail.com>
2025-05-01 22:34:13 +0300

    Серверний чат на сокетах

--- main.py
+++ main.py
@@ -1,53 +1,106 @@
 from fastapi import (
     FastAPI,
-    File,
-    UploadFile,
+    WebSocket,
     HTTPException,
-    BackgroundTasks
+    Depends,
+    Request
+)
+
+from fastapi.security import (
+    OAuth2PasswordBearer
+)
+
+from dotenv import (
+    load_dotenv
 )
 
 from typing import (
     List,
+    Dict
 )
 
-from PIL import (
-    Image
-)
+import uvicorn, jwt, os
 
-import uvicorn, shutil, io
+load_dotenv()
 
 api = FastAPI(docs_url='/')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-def resize_image(image_data: bytes, size: tuple = (1920, 1080)):
-    with Image.open(io.BytesIO(image_data)) as image:
-        image.thumbnail(size)
-        image_bytes = io.BytesIO()
-        image.save(image_bytes, format=image.format)
-        return image_bytes.getvalue()
+print(jwt.encode({'test': 'test'}, SECRET_KEY, algorithm='HS256'))
+
+active_users: List[WebSocket] = []
+
+auth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+def verify_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
     
-async def proces_resizing(path: str):
-    with open(path, 'rb') as f:
-        resized_image = resize_image(f.read())
-    with open(path, 'wb') as file:
-        file.write(resized_image)
+@api.websocket('/ws/chat')
+async def chat(websocket: WebSocket):
+    token = websocket.query_params.get('token')
 
-@api.post('/upload/photo')
-async def upload_phot(bgt: BackgroundTasks, files: List[UploadFile] = File(...)):
-    if len(files) >= 1:
-        for file in files:    
-            with open(f'temp/{file.filename}', 'wb') as f:       
-                if file.content_type in ['image/jpeg', 'image/png', 'image/jpg']:
-                    if file.size < 7 * 1024 * 1024:
-                        f.write(file.file.read())
-                        
-                        bgt.add_task(proces_resizing, f'temp/{file.filename}')
-                    else:
-                        raise HTTPException(status_code=413, detail='Файл занадто великий')  
-                else:
-                    raise HTTPException(status_code=415, detail='Цей тип даних не підтримується')
-            return {'message': f'Файли {file.filename} успішно надіслано'}
-    else:
-        raise HTTPException(status_code=400, detail='Ви не надіслали жодного файлу')
+    if not token:
+        await websocket.close(1008)
+        raise HTTPException(status_code=401, detail='Token not provided')
+
+    try:
+        verify_token(token)
+    except jwt.PyJWTError as e:
+        await websocket.close()
+        raise HTTPException(status_code=401, detail='Invalid token')
+    
+    await websocket.accept()
+    active_users.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            for user in active_users:
+                if user != websocket:
+                    await user.send_text(data)
+
+    except Exception as e:
+        print(f'СТалася помилка: {e}')
+
+    finally:
+        active_users.remove(websocket)
+        await websocket.close()
+
+active_chats: Dict[str, WebSocket] = {}
+
+@api.websocket('/ws/chat/with/user')
+async def chat_by_id(websocket: WebSocket):
+    url_data = websocket.query_params
+
+    token, my_id, user_id = url_data.get('token'), url_data.get('my_id'), url_data.get('user_id')
+
+    if not token or not my_id or not user_id:
+        await websocket.close()
+        raise HTTPException(status_code=400, detail="Missing token or ids")
+
+    try:
+        verify_token(token)
+    except jwt.PyJWTError as e:
+        await websocket.close()
+        raise HTTPException(status_code=401, detail='Invalid token')
+    
+    await websocket.accept()
+    active_chats[my_id] = websocket
+ 
+    print(f'Active chats: {active_chats}')
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            user = active_chats[user_id]
+
+            if user:
+                await user.send_text(f'{my_id}: {data}')
+        
+    except Exception as e:
+        print(f'Салася помилка: {e}')
 
 
 if __name__ == '__main__':
