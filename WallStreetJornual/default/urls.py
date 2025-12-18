2989b80f8bae98569fb851275e035b71b5bfb29a
D1ther <maxnozhkin2006@gmail.com>
2025-12-18 23:01:45 +0200

    commit 23:01

--- WallStreetJornual/default/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("events/", views.show_events, name="show_events"),
-    path("events/add/", views.add_event, name="add_event"),
-    path("events/<int:event_id>/edit/", views.edit_event, name="edit_event")
-]
