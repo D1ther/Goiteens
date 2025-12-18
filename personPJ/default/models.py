186f54215e26eef08222aafa432c6ec4e063ce95
D1ther <maxnozhkin2006@gmail.com>
2025-12-19 01:01:34 +0200

    1:01

--- personPJ/default/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
-    birthday = models.DateField()
-    age = models.IntegerField()
-    mail = models.CharField(max_length=100)
